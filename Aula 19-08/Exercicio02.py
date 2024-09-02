a893c0c9e0473ee23f9b6b03c4f76904511f521d
Arthur Zanon <zanondea@gmail.com>
2024-09-02 19:59:09 -0300

    tempo

--- Aula 19-08/Exercicio02.py
+++ Aula 19-08/Exercicio02.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def bolha(lista):
     n = len(lista)
     for j in range(n):
@@ -7,9 +10,41 @@ def bolha(lista):
     return lista
 
 def main():
-    lista = [8, 7, 15, 12, 18, 24, 23, 10]
-    print("Lista Original:", lista)
-    lista = bolha(lista)
-    print("Lista Ordenada:", lista)
+    lista1 = [8, 7, 15, 12, 18, 24, 23, 10]
+    lista2 = [ 3, 4, 5, 6, 7, 8]
+    lista3 = [4, 4, 7, 2, 9, 7]
+    lista4 = [9,8,7,6,5,4]
+
+
+    lista = bolha(lista1)
+    startTime = datetime.datetime.now()  
+    print("Lista Ordenada:", lista1)
+    endTime = datetime.datetime.now()  
+    time_original = (endTime - startTime).total_seconds()
+    print(f"Tempo para imprimir o array ordenado: {time_original} segundos")
+
+
+    lista = bolha(lista2)
+    startTime = datetime.datetime.now()  
+    print("Lista Ordenada:", lista2)
+    endTime = datetime.datetime.now()  
+    time_original = (endTime - startTime).total_seconds()
+    print(f"Tempo para imprimir o array já ordenado: {time_original} segundos")
+
+
+    lista = bolha(lista3)
+    startTime = datetime.datetime.now()  
+    print("Lista Ordenada:", lista3)
+    endTime = datetime.datetime.now()  
+    time_original = (endTime - startTime).total_seconds()
+    print(f"Tempo para imprimir o array duplicado: {time_original} segundos")
+
+
+    lista = bolha(lista4)
+    startTime = datetime.datetime.now()  
+    print("Lista Ordenada:", lista3)
+    endTime = datetime.datetime.now()  
+    time_original = (endTime - startTime).total_seconds()
+    print(f"Tempo para imprimir o array invertido: {time_original} segundos")
 
 main()  
--- Aula 19-08/MergeSort.py
+++ Aula 19-08/MergeSort.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def merge(v, ini, meio, fim):
     temp = []
     p1 = ini
@@ -12,7 +15,7 @@ def merge(v, ini, meio, fim):
             temp.append(v[p2])
             p2 += 1
     
-    
+
     while p1 <= meio:
         temp.append(v[p1])
         p1 += 1
@@ -35,12 +38,41 @@ def merge_sort(v, ini, fim):
 
 
 def main():
-    v = [38, 27, 43, 3, 9, 82, 10]
-    print("Array original:", v)
+    v = [8, 7, 15, 12, 18, 24, 23, 10]
+    v2 = [ 3, 4, 5, 6, 7, 8]
+    v3 = [4, 4, 7, 2, 9, 7]
+    v4 = [9,8,7,6,5,4]
+    
 
     merge_sort(v, 0, len(v) - 1)
-
+    startTime = datetime.datetime.now()  
     print("Array ordenado:", v)
+    endTime = datetime.datetime.now() 
+    time_sorted = (endTime - startTime).total_seconds()
+    print(f"Tempo para imprimir o array ordenado: {time_sorted} segundos")
+
+
+    merge_sort(v2, 0, len(v2) - 1)
+    startTime = datetime.datetime.now()  
+    print("Array ordenado:", v2)
+    endTime = datetime.datetime.now() 
+    time_sorted = (endTime - startTime).total_seconds()
+    print(f"Tempo para imprimir o array já ordenado: {time_sorted} segundos")
+
+    merge_sort(v3, 0, len(v3) - 1)
+    startTime = datetime.datetime.now()  
+    print("Array ordenado:", v3)
+    endTime = datetime.datetime.now() 
+    time_sorted = (endTime - startTime).total_seconds()
+    print(f"Tempo para imprimir o array duplicado: {time_sorted} segundos")
+
+
+    merge_sort(v4, 0, len(v4) - 1)
+    startTime = datetime.datetime.now()  
+    print("Array ordenado:", v4)
+    endTime = datetime.datetime.now() 
+    time_sorted = (endTime - startTime).total_seconds()
+    print(f"Tempo para imprimir o array invertido: {time_sorted} segundos")
 
 if __name__ == "__main__":
     main()
